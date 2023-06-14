2ec91379a316949718c54fba68b20c91646c8bd9
rishabh <rishabhgarg875@gmail.com>
2023-06-14 22:33:44 +0530

    comment

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-// Smooth scrolling
-$('a.nav-link').on('click', function (e) {
-  if (this.hash !== '') {
-      e.preventDefault();
+// // Smooth scrolling
+// $('a.nav-link').on('click', function (e) {
+//   if (this.hash !== '') {
+//       e.preventDefault();
 
-      const hash = this.hash;
-      $('html, body').animate(
-          {
-              scrollTop: $(hash).offset().top - 70
-          },
-          800
-      );
-  }
-});
+//       const hash = this.hash;
+//       $('html, body').animate(
+//           {
+//               scrollTop: $(hash).offset().top - 70
+//           },
+//           800
+//       );
+//   }
+// });
 
 // Close responsive navbar on click
-$('.navbar-nav>li>a').on('click', function () {
-  $('.navbar-collapse').collapse('hide');
-});
+// $('.navbar-nav>li>a').on('click', function () {
+//   $('.navbar-collapse').collapse('hide');
+// });
